60051f04363eaa9748df642f3238945bc9016f0d
LuizGustavo10 <gustavo180g@gmail.com>
2023-09-30 17:23:51 -0300

    Feita exclusão, edição ainda está em andamento

--- rede social 2/principal.php
+++ rede social 2/principal.php
@@ -33,14 +33,50 @@
             $_SESSION['usuario'] = $_POST['usuario'];
         }
 
-        $sql = "SELECT usuario, mensagem FROM tabela_mensagens";
-        $result = $conn->query($sql);
+             // Se for POST e não estiver vazio, exclusão da mensagem
+             if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['excluir']) && isset($_POST['mensagem_id'])) {
+                $mensagem_id = $_POST['mensagem_id'];
+                $sql = "DELETE FROM tabela_mensagens WHERE id = $mensagem_id";
+                if ($conn->query($sql) === TRUE) {
+                    // Redirecione de volta para a página do chat após a exclusão
+                    header("Location: principal.php");
+                    exit();
+                } else {
+                    echo "Erro ao excluir mensagem: " . $conn->error;
+                }
+            }
 
+        $sql = "SELECT usuario, mensagem, id FROM tabela_mensagens";
+        $result = $conn->query($sql);
+        
+                // // Se for GET e tiver um parâmetro "editar" na URL, carregue a mensagem para edição
+                // if (isset($_GET['editar'])) {
+                //     $mensagem_id = $_GET['editar'];
+                //     $sql = "SELECT mensagem FROM tabela_mensagens WHERE id = $mensagem_id";
+                //     $result = $conn->query($sql);
+                //     if ($result->num_rows == 1) {
+                //         $row = $result->fetch_assoc();
+                //         $mensagem_existente = $row['mensagem'];
+                //     } else {
+                //         echo "Mensagem não encontrada.";
+                //         exit();
+                //     }
+                // }
         
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo '<div class="mensagens">';
-                echo "<p><strong>{$row['usuario']}:</strong> {$row['mensagem']}</p> ";
+                echo "<p><strong>{$row['usuario']}:</strong> {$row['mensagem']}</p>";
+
+                // Adicione um link "Editar" que redireciona para a própria página com o ID da mensagem
+                echo '<a href="principal.php?editar=' . $row['id'] . '">Editar</a>';
+
+                // Adicione um formulário para ação de exclusão da mensagem
+                echo '<form method="post" action="">';
+                echo "<input type='hidden' name='mensagem_id' value='{$row['id']}' />";
+                echo "<button type='submit' name='excluir'>Excluir</button>";
+                echo '</form>';
+
                 echo '</div>';
             }
         } else {
